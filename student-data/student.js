c539b2fc9d00e58fe94f81e69bc6824726c833da
Jamie Ahmed <jamieahmed273@gmail.com>
2022-06-14 08:40:58 -0400

    updated

---
+++ student-data/student.js
@@ -0,0 +1,12 @@
+const students = [
+    { text: 'Jamie', done: true, _id: 125223 },
+    { text: 'dali', done: false, _id: 127904 },
+    { text: 'salih', done: false, _id: 139608 },
+    { text: 'Jamie', done: true, _id: 125223 },
+    { text: 'dali', done: false, _id: 127904 },
+    { text: 'salih', done: false, _id: 139608 },
+]
+
+export {
+    students
+}
